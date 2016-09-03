71c4eea447ccc94da9e9eb1d7cc902d99e5e0b95
b <a>
2016-09-03 23:24:37 +0700

    detect change, build params

--- app/dashboard/dashboard.component.ts
+++ app/dashboard/dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnChanges, SimpleChange } from '@angular/core';
+import { Component, OnChanges, SimpleChange, Input, DoCheck } from '@angular/core';
 
 import { Pair } from './pair';
 
@@ -6,8 +6,9 @@ import { Pair } from './pair';
   selector: 'my-dashboard',
   templateUrl: 'app/dashboard/dashboard.html'
 })
-export class DashboardComponent {
-	pairs: Pair[] = [
+export class DashboardComponent implements DoCheck {
+	params : string = "";
+	@Input() pairs: Pair[] = [
 		{
 			name: "a",
 			value: 9,
@@ -29,7 +30,25 @@ export class DashboardComponent {
 			min: 1,
 			max: 10
 		}
-	]
+	];
+	ngDoCheck() {
+		this.params = this.buildParams(this.pairs);
+		console.log(this.params);
+	}
 
+	buildParams(pairs : Pair[]) {
 
+		let params : string = "";
+		pairs.forEach(function (obj, i) {
+			let name = obj.name;
+			let value = obj.value;
+
+			let tmp = name + "=" + value;
+			params += tmp;
+			if (i < (pairs.length - 1)) {
+				params += "&";
+			}
+		});
+		return params;
+	}
 }
