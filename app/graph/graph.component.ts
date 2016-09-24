aa9e04ee8888ae33df81116e2a3bb13275d7f759
b <a>
2016-09-24 15:35:31 +0700

    stylesheet

--- app/graph/graph.component.ts
+++ app/graph/graph.component.ts
@@ -25,8 +25,17 @@ export class GraphComponent {
 		});
 	}
 	public barChartOptions:any = {
-	    scaleShowVerticalLines: false,
-	    responsive: true
+	    scales: {
+	    	yAxes: [
+	    		{
+	    			ticks: {
+	    				beginAtZero: true
+	    			}
+	    		}
+	    	]
+	    },
+	    isFixedWidth:false,
+	    barWidth:20
 	};
 
 	 public barChartType:string = 'bar';
@@ -38,31 +47,8 @@ export class GraphComponent {
 
 	// events
 	public chartClicked(e:any):void {
-	   console.log(e);
 	}
 
 	public chartHovered(e:any):void {
-	   console.log(e);
 	}
-
-  public randomize():void {
-    // Only Change 3 values
-    let data = [
-      Math.round(Math.random() * 100),
-      59,
-      80,
-      (Math.random() * 100),
-      56,
-      (Math.random() * 100),
-      40];
-    let clone = JSON.parse(JSON.stringify(this.barChartData));
-    clone[0].data = data;
-    this.barChartData = clone;
-    /**
-     * (My guess), for Angular to recognize the change in the dataset
-     * it has to change the dataset variable directly,
-     * so one way around it, is to clone the data, change it and then
-     * assign it;
-     */
-  }
 }
